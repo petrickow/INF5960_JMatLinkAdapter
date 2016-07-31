13f2ef04c47bbbff0dc1ec8b95d5da89a972077f
Cato Danielsen <catodanielsen@gmail.com>
2016-07-31 19:28:32 +0200

    added simple tests for execution

--- src/test/java/no/uio/taco/pukaMatControl/evaluation/EvaluationTests.java
+++ src/test/java/no/uio/taco/pukaMatControl/evaluation/EvaluationTests.java
@@ -2,33 +2,44 @@ package no.uio.taco.pukaMatControl.evaluation;
 
 import static org.junit.Assert.*;
 import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 
 public class EvaluationTests {
-//					[FP, TP, TP, TP, FP , TP ] 
-	static int[] result = {10, 30, 50, 70, 100, 110};
-//					  [FN, TP, TP, TP, FN, TP,  FN ]
-	static int[] reference = {15, 30, 50, 70, 90, 100, 120};
-	static int countTN = 150;
-	
+//							[FP, TP, TP, TP, FP , TP ] 
+	final int[][] result = 	{{15, 30, 50, 70, 90, 100, 120},{10, 30, 50, 70, 100, 110}};
+//					  			[FN, TP, TP, TP, FN, TP,  FN ]
+	final int[] reference =	 {15, 30, 50, 70, 90, 100, 120};
+	final int countTN = 150;
+	static int i;
+	EvaluationResult e; 
 	
 	@BeforeClass
-	public static void setup() {
+	public static void  setup() {
 		System.out.println("Create test-data set "); 
-		Evaluation.evaluateAnalysisResults(result, reference, countTN);
+		i = 0;
 	}
-		
+	
+	@Before
+	public void beforeEach() {
+		e = Evaluation.evaluateAnalysisResults(result[i++], reference, countTN);
+	}
 	/**
 	 */
 	@Test
-	public void CalculatePrecision() {
-		
+	public void IdenticalArrayesGives100() {
+		Assert.assertNotNull(e);
+		assertTrue(e.precision == 1);
+		assertTrue(e.recall == 1);
 	}	
 	
 	@Test
-	public void CalculateRecall() {
-		
+	public void IsLowerThanOneWhenThereAreDifferences() {
+		Assert.assertNotNull(e);
+		assertTrue(e.precision < 1);
+		assertTrue(e.recall < 1);
 	}	
 }
