8d60f99c098f865d96706a2eb0f0cf6355d68bf4
Cato Danielsen <catodanielsen@gmail.com>
2016-08-01 21:03:23 +0200

    added generated signals, cleanup in main folder

--- src/test/java/no/uio/taco/pukaMatControl/evaluation/EvaluationTests.java
+++ src/test/java/no/uio/taco/pukaMatControl/evaluation/EvaluationTests.java
@@ -1,7 +1,6 @@
 package no.uio.taco.pukaMatControl.evaluation;
 
 import static org.junit.Assert.*;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -9,10 +8,10 @@ import org.junit.Test;
 
 
 public class EvaluationTests {
-//							[FP, TP, TP, TP, FP , TP ] 
+// cheatsheet for results:									[FP, TP, TP, TP, FP , TP ] 
 	final int[][] result = 	{{15, 30, 50, 70, 90, 100, 120},{10, 30, 50, 70, 100, 110}};
-//					  			[FN, TP, TP, TP, FN, TP,  FN ]
-	final int[] reference =	 {15, 30, 50, 70, 90, 100, 120};
+//					  		[FN, TP, TP, TP, FN, TP,  FN ]
+	final int[] reference =	{15, 30, 50, 70, 90, 100, 120};
 	final int countTN = 150;
 	static int i;
 	EvaluationResult e; 
